50dbfc70acaaea89e964141567714ab882e8565a
Parinay Raya <parinayraya7432@gmail.com>
2025-03-13 15:32:27 +0530

    bcryptjs updated

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -47,7 +47,7 @@ export const login = async (req, res) => {
             return res.status(401).json({ message: "User not found" });
         }
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcryptjs.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({ message: "Invalid password" });
         }
